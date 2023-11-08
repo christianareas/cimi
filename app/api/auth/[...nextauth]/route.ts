6e9fa8efc47192ec4d6443d2ca9343e80a9e58f8
Christian Areas <me@areas.me>
2023-11-07 19:20:47 -0800

    Added real auth logic.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 // Dependencies.
 import type { NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
+import { prisma } from "@/app/lib/prisma"
+import { compare } from "bcryptjs"
 import NextAuth from "next-auth"
 
 // Configure NextAuth.
@@ -22,9 +24,36 @@ export const authOptions: NextAuthOptions = {
 				},
       },
       async authorize(credentials) {
-        // Replace with logic to authenticate user. **
-        const user = { id: "1", name: "Christian", email: "me@areas.me" }
-        return user
+        // If there’s no email or password, return null.
+        if (!credentials?.email || !credentials?.password) {
+          return null
+        }
+
+        // Find the user by their email.
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email
+          },
+        })
+
+        // If there’s no user, return null.
+        if (!user) {
+          return null
+        }
+
+        // If the password is incorrect, return null.
+        if (!(await compare(credentials.password, user.password))) {
+          return null
+        }
+        
+        // Return the user.
+        return {
+          id: user.id,
+          email: user.email,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          randomKey: "I am a randomKey, and so can you.",
+        }
       },
     }),
   ],
