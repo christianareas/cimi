700d9e33a4aba2fef20a37bd7267370bf2eade9e
Christian Areas <me@areas.me>
2023-11-09 19:27:25 -0800

    Added API route protection example.

--- app/api/session/route.ts
+++ app/api/session/route.ts
@@ -5,8 +5,23 @@ import { NextResponse } from "next/server"
 
 // Get the session from an API route.
 export async function GET(request: Request) {
+	// Get the session.
 	const session = await getServerSession(authOptions)
 
+	// If there’s no session, return a 401 error.
+	if (!session) {
+		return new NextResponse(
+			JSON.stringify({
+				status: "fail",
+				message: "You’re not authenticated.",
+			}),
+			{
+				status: 401
+			},
+		)
+	}
+
+	// Return the session.
 	return NextResponse.json({
 		authenticated: !!session,
 		session,
