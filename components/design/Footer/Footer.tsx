3a8ee95722aa18155e35e6f9f9c265a638e1f000
Christian Areas <me@areas.me>
2024-07-13 15:49:49 -0700

    Added Footer component.

---
+++ components/design/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+// Dependencies.
+
+// Footer component.
+export default function Footer() {
+	// Render.
+	return (
+		<footer className="bg-cimi-purple text-cimi-cream p-10">
+			<section className="max-w-screen-lg mx-auto">
+				<p>Footer</p>
+			</section>
+		</footer>
+	)
+}
