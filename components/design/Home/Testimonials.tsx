58c82a67c40dc84c3827c51a52947777217e84b0
Christian Areas <me@areas.me>
2024-09-06 23:25:15 -0700

    Finished styling testimonials.

--- components/design/Home/Testimonials.tsx
+++ components/design/Home/Testimonials.tsx
@@ -7,45 +7,49 @@ import Image from "next/image"
 export default function Testimonials() {
 	// Render.
 	return (
-		<article className="space-y-3 bg-cimi-red p-20 text-center font-normal">
-			<h2 className="font-ancho font-bold text-2xl text-cimi-cream">
-				Testimonials of Members
-			</h2>
-			<Tabs.Root defaultValue="tab-one">
-				<Tabs.List>
-					<Tabs.Trigger value="tab-one" />
-					<Tabs.Trigger value="tab-two" />
-				</Tabs.List>
-				<Tabs.Content value="tab-one">
-					<article className="mx-auto grid grid-cols-4 items-center text-left text-cimi-cream">
-						<ChevronLeftIcon className="ml-auto h-8 w-8 cursor-pointer text-cimi-cream" />
-						<section className="px-10">
-							<Image
-								src="/images/home/testimonials/maria-perla.jpg"
-								alt="Maria Perla"
-								width="800"
-								height="800"
-								className="ml-auto rounded-lg"
-							/>
-						</section>
-						<section className="px-10">
-							<p className="font-ancho text-2xl">“</p>
-							<p>
-								Being a member for more than five years has been an opportunity
-								for me because it has helped me physically, emotionally, and
-								financially and has helped me manage stress. It has helped me
-								learn about myself.
-							</p>
-							<p className="font-ancho text-2xl">”</p>
-							<p className="font-bold text-xs">–Mom</p>
-						</section>
-						<ChevronRightIcon className="mr-auto h-8 w-8 cursor-pointer text-cimi-cream" />
-					</article>
-				</Tabs.Content>
-				<Tabs.Content value="tab-two">
-					<p>Tab two.</p>
-				</Tabs.Content>
-			</Tabs.Root>{" "}
+		<article className="bg-cimi-red px-10 py-20 text-center font-normal lg:text-left">
+			<section className="lg:mx-auto lg:max-w-screen-lg">
+				<h2 className="pb-5 text-center font-ancho font-bold text-2xl text-cimi-cream">
+					Testimonials of Members
+				</h2>
+				<Tabs.Root defaultValue="tab-one">
+					<Tabs.List className="hidden">
+						<Tabs.Trigger value="tab-one" />
+						<Tabs.Trigger value="tab-two" />
+					</Tabs.List>
+					<Tabs.Content value="tab-one">
+						<article className="mx-auto grid grid-cols-[1fr,5fr,1fr] items-center py-2 text-cimi-cream">
+							<ChevronLeftIcon className="mr-3 ml-auto h-8 w-8 cursor-pointer text-cimi-cream" />
+							<section className="lg:grid lg:grid-cols-2 lg:gap-5">
+								<section className="relative mb-5 w-full pb-[100%] lg:mb-0">
+									<Image
+										src="/images/home/testimonials/maria-perla.jpg"
+										alt="Maria Perla"
+										fill
+										className="absolute inset-0 mx-auto mb-5 rounded-lg object-cover lg:mb-0"
+									/>
+								</section>
+								<section className="">
+									<p className="font-ancho text-2xl">“</p>
+									<p>
+										In these 3 years of being a member, I am part of a great
+										community that helps girls learn how to code and be a better
+										person.
+									</p>
+									<p className="pt-5 font-ancho text-2xl">”</p>
+									<p className="font-bold text-xs">
+										–High school-aged daughter
+									</p>
+								</section>
+							</section>
+							<ChevronRightIcon className="mr-auto ml-3 h-8 w-8 cursor-pointer text-cimi-cream" />
+						</article>
+					</Tabs.Content>
+					<Tabs.Content value="tab-two">
+						<p>Tab two.</p>
+					</Tabs.Content>
+				</Tabs.Root>{" "}
+			</section>
 		</article>
 	)
 }
