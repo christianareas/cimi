91dda69d0669d1a0d904b9d9c0241c5de059f8a9
Christian Areas <me@areas.me>
2024-09-06 23:33:09 -0700

    Added better spacing to testimonial buttons.

--- components/design/Home/Testimonials.tsx
+++ components/design/Home/Testimonials.tsx
@@ -19,8 +19,8 @@ export default function Testimonials() {
 					</Tabs.List>
 					<Tabs.Content value="tab-one">
 						<article className="mx-auto grid grid-cols-[1fr,5fr,1fr] items-center py-2 text-cimi-cream">
-							<ChevronLeftIcon className="mr-3 ml-auto h-8 w-8 cursor-pointer text-cimi-cream" />
-							<section className="lg:grid lg:grid-cols-2 lg:gap-5">
+							<ChevronLeftIcon className="mr-3 ml-auto h-8 w-8 cursor-pointer text-cimi-cream md:mr-10 lg:mr-20" />
+							<section className="lg:grid lg:grid-cols-2 lg:gap-10">
 								<section className="relative mb-5 w-full pb-[100%] lg:mb-0">
 									<Image
 										src="/images/home/testimonials/maria-perla.jpg"
@@ -42,7 +42,7 @@ export default function Testimonials() {
 									</p>
 								</section>
 							</section>
-							<ChevronRightIcon className="mr-auto ml-3 h-8 w-8 cursor-pointer text-cimi-cream" />
+							<ChevronRightIcon className="mr-auto ml-3 h-8 w-8 cursor-pointer text-cimi-cream md:ml-10 lg:ml-20" />
 						</article>
 					</Tabs.Content>
 					<Tabs.Content value="tab-two">
