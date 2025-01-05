68d7e93e9bd3b1b480f57f362af22f869221b10e
Christian Areas <me@areas.me>
2025-01-05 00:00:39 -0800

    Added lib to filter and map campaign event data.

--- src/app/api/givebutterEvents/route.ts
+++ src/app/api/givebutterEvents/route.ts
@@ -1,6 +1,7 @@
 // Dependencies.
 import { NextResponse } from "next/server"
 import fetchGivebutterData from "@/lib/fetchGivebutterData"
+import filterAndMapCampaignEvents from "@/lib/filterAndMapCampaignEvents"
 
 // Types.
 import type { NextRequest } from "next/server"
@@ -8,10 +9,15 @@ import type { NextRequest } from "next/server"
 // GET request.
 export async function GET(request: NextRequest) {
 	try {
+		// Fetch.
 		const response = await fetchGivebutterData("/campaigns")
 		const data = response.data
 
-		return NextResponse.json({ data }, { status: 200 })
+		// Filter and map.
+		const campaignEvents = filterAndMapCampaignEvents(data)
+
+		// Return.
+		return NextResponse.json({ campaignEvents }, { status: 200 })
 	} catch (error) {
 		console.error(error)
 		return NextResponse.json(
---
+++ src/lib/filterAndMapCampaignEvents.ts
@@ -0,0 +1,89 @@
+// Types.
+type CampaignEvent = {
+	id: number
+	code: string
+	account_id: string
+	event_id: number | null
+	type: string
+	title: string
+	subtitle: string | null
+	description: string | null
+	slug: string
+	url: string
+	goal: number | null
+	raised: number
+	donors: number
+	currency: string
+	cover: { url: string; type: string; source: string } | null
+	status: string
+	timezone: string
+	end_at: string | null
+	event?: {
+		title: string
+		type: string
+		location_name: string | null
+		address_formatted: string | null
+		google_place_id: string | null
+		start_at: string | null
+		end_at: string | null
+		timezone: string
+		details: string | null
+		private: boolean
+		tickets_required: boolean
+		livestream: string | null
+		livestream_start_at: string | null
+		livestream_end_at: string | null
+	}
+	created_at: string
+	updated_at: string
+}
+
+// Filter and map campaign events.
+export default function filterAndMapCampaignEvents(data: CampaignEvent[]) {
+	return data
+		.filter((campaignEvent) => campaignEvent.type === "event")
+		.map((campaignEvent) => ({
+			// Campaign.
+			campaignId: campaignEvent.id,
+			campaignStatus: campaignEvent.status,
+
+			campaignTitle: campaignEvent.title,
+			campaignSubtitle: campaignEvent.subtitle,
+			campaignCover: campaignEvent.cover,
+			campaignDescription: campaignEvent.description,
+
+			campaignSlug: campaignEvent.slug,
+			campaignUrl: campaignEvent.url,
+
+			campaignCurrency: campaignEvent.currency,
+			campaignGoal: campaignEvent.goal,
+			campaignRaised: campaignEvent.raised,
+			campaignDonors: campaignEvent.donors,
+
+			campaignTimezone: campaignEvent.timezone,
+			campaignCreatedAt: campaignEvent.created_at,
+			campaignUpdatedAt: campaignEvent.updated_at,
+			campaignEndAt: campaignEvent.end_at,
+
+			// Event.
+			eventId: campaignEvent.event_id,
+			eventType: campaignEvent.event?.type,
+			eventTitle: campaignEvent.event?.title,
+			eventDetails: campaignEvent.event?.details,
+
+			eventPrivate: campaignEvent.event?.private,
+			eventTicketsRequired: campaignEvent.event?.tickets_required,
+
+			eventAddress: campaignEvent.event?.location_name,
+			eventAddressFormatted: campaignEvent.event?.address_formatted,
+			eventGooglePlaceId: campaignEvent.event?.google_place_id,
+
+			eventTimezone: campaignEvent.event?.timezone,
+			eventStartAt: campaignEvent.event?.start_at,
+			eventEndAt: campaignEvent.event?.end_at,
+
+			eventLivestream: campaignEvent.event?.livestream,
+			eventLivestreamStartAt: campaignEvent.event?.livestream_start_at,
+			eventLivestreamEndAt: campaignEvent.event?.livestream_end_at,
+		}))
+}
