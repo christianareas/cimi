7e5a4a92ea684363c662512d9be402b9a03ca7c4
Christian Areas <me@areas.me>
2025-01-07 00:38:40 -0800

    Turned on cache for /api/markdown calls.

--- src/components/Shared/ContentCard.tsx
+++ src/components/Shared/ContentCard.tsx
@@ -39,7 +39,7 @@ export default async function ContentCard({
 }: ContentCardProps) {
 	const { markdown } = await fetchData(
 		`/api/markdown?src=${contentSrc}`,
-		"no-cache",
+		"cache",
 	)
 
 	// Set up the classes.
