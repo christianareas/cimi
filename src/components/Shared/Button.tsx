7ed2f4258a68544d307add54f8edd194200477e3
Christian Areas <me@areas.me>
2025-02-02 20:57:11 -0800

    Refactored how color classes are generated.

--- src/components/Shared/Button.tsx
+++ src/components/Shared/Button.tsx
@@ -4,20 +4,25 @@ import Link from "next/link"
 // Types.
 type ButtonProps = {
 	buttonText: string
-	colorScheme: string
+	colorScheme: "cimi-blue" | "cimi-green"
 }
 
 // Component.
 export default function Button({ buttonText, colorScheme }: ButtonProps) {
+	// Colors.
+	const colorsSchemes = {
+		"cimi-blue": "border-cimi-blue shadow-cimi-blue text-cimi-blue",
+		"cimi-green": "border-cimi-green shadow-cimi-green text-cimi-green",
+	}
+	const colorClasses = colorsSchemes[colorScheme]
+
 	// Render.
 	return (
 		<Link href="#todo">
 			<button
 				type="button"
 				className={[
-					`border-${colorScheme}`,
-					`shadow-${colorScheme}`,
-					`text-${colorScheme}`,
+					`${colorClasses}`,
 					"mt-5 w-40 cursor-pointer rounded-xl border-2 px-4 py-3 text-center font-ancho font-medium text-xs shadow-[4px_4px_0]",
 				]
 					.filter(Boolean)
