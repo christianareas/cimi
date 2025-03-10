48fee888f71f6461c433ace565ede375c8635ac3
Christian Areas <me@areas.me>
2025-03-09 21:41:53 -0700

    Updated event selector style.

--- src/components/Events/UpcomingAndPastEvents.tsx
+++ src/components/Events/UpcomingAndPastEvents.tsx
@@ -79,38 +79,41 @@ export default function UpcomingAndPastEvents() {
 
 	// Render.
 	return (
-		<section className="px-10 pb-10 sm:px-20">
+		<section className="px-10 pb-10 sm:px-20 sm:pb-20">
 			<section className="mx-auto">
-				{/* Event selector. */}
-				<section className="flex justify-center">
+				{/* Event selector and options. */}
+				<section className="flex justify-center pb-10 sm:pb-20">
 					<Select.Root
 						value={selectedEvents.toString()}
 						onValueChange={(value) => setSelectedEventType(Number(value))}
 					>
+						{/* Selector. */}
 						<Select.Trigger
-							className="flex w-96 justify-between rounded-lg border border-cimi-red-orange py-1 pr-3 pl-4 font-ancho font-bold text-2xl text-cimi-red-orange"
+							className="flex items-center rounded-lg border border-cimi-red-orange bg-cimi-cream pt-1 pr-3 pb-2 pl-4 font-ancho font-bold text-2xl text-cimi-red-orange"
 							aria-label="Upcoming or Past Events"
 						>
 							<Select.Value>
-								<h2 className="pt-1">
+								<h2 className="pt-2">
 									{upcomingAndPastEvents[selectedEventType].eventsHeading}
 								</h2>
 							</Select.Value>
 							<Select.Icon>
-								<ChevronDownIcon className="h-8 w-8 text-cimi-red-orange" />
+								<ChevronDownIcon className="ml-2 h-8 w-8 text-cimi-red-orange" />
 							</Select.Icon>
 						</Select.Trigger>
+
+						{/* Options. */}
 						<Select.Portal>
-							<Select.Content>
-								<Select.Viewport className="rounded-lg border border-cimi-red-orange bg-cimi-cream px-4 py-2 font-ancho font-bold text-2xl text-cimi-red-orange">
+							<Select.Content className="absolute right-0 left-0 mx-auto">
+								<Select.Viewport className="flex flex-col rounded-lg border border-cimi-red-orange bg-cimi-cream px-4 py-2 font-ancho font-bold text-2xl text-cimi-red-orange">
 									{upcomingAndPastEvents.map((event, index) => (
 										<Select.Item
 											key={event.eventsType}
 											value={index.toString()}
 										>
-											<h2 className="pt-1">
-												<Select.ItemText>{event.eventsHeading}</Select.ItemText>
-											</h2>
+											<Select.ItemText>
+												<h2 className="pt-2">{event.eventsHeading}</h2>
+											</Select.ItemText>
 										</Select.Item>
 									))}
 								</Select.Viewport>
@@ -120,60 +123,62 @@ export default function UpcomingAndPastEvents() {
 				</section>
 
 				{/* Events */}
-				{selectedEvents.events.length === 0 ? (
-					<p className="flex justify-center p-5 text-gray-500 text-xs italic">
-						No {selectedEvents.eventsType.replace("-", " ")}.
-					</p>
-				) : (
-					selectedEvents.events.map((campaign) => (
-						<article
-							className="fade py-10 font-medium lg:mx-auto lg:flex lg:max-w-(--breakpoint-lg)"
-							key={campaign.campaignId}
-						>
-							<section className="pb-10 lg:w-1/2 lg:pr-10">
-								{campaign.campaignCoverType === "image" &&
-								campaign.campaignCoverUrl ? (
-									<section className="relative aspect-video">
-										<Link href={campaign.campaignUrl}>
-											<Image
-												src={campaign.campaignCoverUrl}
-												alt="Learn More"
-												fill
-												className="rounded-lg object-cover"
-											/>
-										</Link>
-									</section>
-								) : null}
-							</section>
-
-							<section className="lg:w-1/2 lg:pl-10">
-								<h3 className="font-ancho font-bold text-cimi-red-orange text-xl">
-									{campaign.eventTitle}
-								</h3>
-
-								{campaign.eventStartAt ? (
-									<h4 className="font-ancho font-medium text-cimi-red-orange">
-										{campaign.eventEndAt
-											? `${campaign.eventStartAt.replace(/\s*[A-Z]{2,4}$/, "")} – ${campaign.eventEndAt}`
-											: campaign.eventStartAt}
-									</h4>
-								) : null}
-
-								<section className="pt-4 pb-10">
-									{campaign.eventDetails}
+				<section className="space-y-10 sm:space-y-20">
+					{selectedEvents.events.length === 0 ? (
+						<p className="flex justify-center p-5 text-gray-500 text-xs italic">
+							No {selectedEvents.eventsType.replace("-", " ")}.
+						</p>
+					) : (
+						selectedEvents.events.map((campaign) => (
+							<article
+								className="fade font-medium lg:mx-auto lg:flex lg:max-w-(--breakpoint-lg)"
+								key={campaign.campaignId}
+							>
+								<section className="pb-10 lg:w-1/2 lg:pr-10">
+									{campaign.campaignCoverType === "image" &&
+									campaign.campaignCoverUrl ? (
+										<section className="relative aspect-video">
+											<Link href={campaign.campaignUrl}>
+												<Image
+													src={campaign.campaignCoverUrl}
+													alt="Learn More"
+													fill
+													className="rounded-lg object-cover"
+												/>
+											</Link>
+										</section>
+									) : null}
 								</section>
 
-								<Button
-									buttonText="Learn More"
-									buttonLink={campaign.campaignUrl}
-									buttonWidth="w-40"
-									buttonClassNames="border-cimi-cream bg-cimi-red-orange text-cimi-cream shadow-cimi-red-orange"
-									buttonType="button"
-								/>
-							</section>
-						</article>
-					))
-				)}
+								<section className="lg:w-1/2 lg:pl-10">
+									<h3 className="font-ancho font-bold text-cimi-red-orange text-xl">
+										{campaign.eventTitle}
+									</h3>
+
+									{campaign.eventStartAt ? (
+										<h4 className="font-ancho font-medium text-cimi-red-orange">
+											{campaign.eventEndAt
+												? `${campaign.eventStartAt.replace(/\s*[A-Z]{2,4}$/, "")} – ${campaign.eventEndAt}`
+												: campaign.eventStartAt}
+										</h4>
+									) : null}
+
+									<section className="pt-4 pb-10">
+										{campaign.eventDetails}
+									</section>
+
+									<Button
+										buttonText="Learn More"
+										buttonLink={campaign.campaignUrl}
+										buttonWidth="w-40"
+										buttonClassNames="border-cimi-cream bg-cimi-red-orange text-cimi-cream shadow-cimi-red-orange"
+										buttonType="button"
+									/>
+								</section>
+							</article>
+						))
+					)}
+				</section>
 			</section>
 		</section>
 	)
