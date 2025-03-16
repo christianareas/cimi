060fe581a43b76dd7d4d174fd6dce7700fa9f2e0
Christian Areas <me@areas.me>
2025-03-16 03:10:34 -0700

    Added API to add Givebutter contact for newsletter subscription.

---
+++ src/app/api/givebutterContacts/route.ts
@@ -0,0 +1,38 @@
+// Dependencies.
+import { NextResponse } from "next/server"
+import fetchGivebutterData from "@/lib/api/fetchGivebutterData"
+
+// POST request.
+export async function POST(request: Request) {
+	try {
+		// Email.
+		const { email } = await request.json()
+		if (!email) {
+			return NextResponse.json({ error: "There’s no email." }, { status: 400 })
+		}
+
+		// Body.
+		const body = {
+			first_name: "_",
+			last_name: "_",
+			emails: [{ type: "personal", value: email }],
+			tags: ["cimiNewsletterSubscriber"],
+		}
+
+		// Fetch.
+		const { primary_email: primaryEmail } = await fetchGivebutterData(
+			"POST",
+			"/contacts",
+			body,
+		)
+
+		// Return.
+		return NextResponse.json({ primaryEmail }, { status: 200 })
+	} catch (error) {
+		console.error(error)
+		return NextResponse.json(
+			{ error: "Couldn't fetch Givebutter data." },
+			{ status: 500 },
+		)
+	}
+}
