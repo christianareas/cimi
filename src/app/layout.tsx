e07962394d0603c2f20ad6074cefb7a097cf5fa1
Christian Areas <me@areas.me>
2026-03-08 16:56:16 -0700

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
 // Dependencies.
 import "./globals.css"
+import { Roboto } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const roboto = Roboto({subsets:['latin'],variable:'--font-sans'});
+
 
 // Metadata.
 export const metadata = {
@@ -14,7 +19,7 @@ export default function RootLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<html lang="en">
+		<html lang="en" className={cn("font-sans", roboto.variable)}>
 			<body className="bg-cimi-cream font-montserrat font-regular text-sm">
 				{children}
 			</body>
