ec8247c8bcecff8596815e15d93f5f2154145e33
Christian Areas <me@areas.me>
2026-03-08 17:20:22 -0700

    Simplified shadcn/ui additions.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,5 @@
 // Dependencies.
 import "./globals.css"
-import { Roboto } from "next/font/google"
-import { cn } from "@/lib/utils"
-
-const roboto = Roboto({ subsets: ["latin"], variable: "--font-sans" })
 
 // Metadata.
 export const metadata = {
@@ -18,7 +14,7 @@ export default function RootLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<html lang="en" className={cn("font-sans", roboto.variable)}>
+		<html lang="en">
 			<body className="bg-cimi-cream font-montserrat font-regular text-sm">
 				{children}
 			</body>
